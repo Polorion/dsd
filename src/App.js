617d89e9f394f78ec61e5dabb9e00136fe502f87
Polorion <97245263+Polorion@users.noreply.github.com>
2022-07-17 19:30:38 +0300

    qw

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import "./App.css";
+import React from "react";
+import StartAppComponent from "./StartAppComponent/StartAppComponent";
+
+function App() {
+  return <StartAppComponent />;
+}
+
+export default App;
